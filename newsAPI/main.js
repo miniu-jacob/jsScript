9d6592a11b2bee904e7dcb7ed50c27df66619826
jacob <miniu.media@gmail.com>
2024-07-09 00:14:09 +0700

    news updated

--- newsAPI/main.js
+++ newsAPI/main.js
@@ -1,4 +1,7 @@
-const API_KEY = `dddc6ed86f8243368ad4fdab7ca2a3c3`;
+import config from '../apikey.js'
+const API_KEY = config.NEWS_API_KEY1;
+// const API_KEY = process.env.NEWS_API_KEY1;
+let news = [];
 
 // 뉴스를 가지고 오는 함수 만들기
 const getNews = async () => {
@@ -10,18 +13,19 @@ const getNews = async () => {
     // new URL() 에서 URL 과 같은 것을 인스턴스라 한다.
 
     // async: 비동기 처리를 하겠다.
-    const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
-    );
+    // const url = new URL(
+    //     `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+    // );
     console.log('API_KEY: ', API_KEY);
     const response = await fetch(url);
     // console.log('uuu', url);
     const data = await response.json();
+    news = data.articles;
     //  data의 내용을 개발자 모드에서 확인해 보면
     // articles, status, totalResults 가 있는 것을 확인할 수 있다. 따라서 articles를 보고 싶다면
     // data.articles 로 쓸 수 있다.
     // 따라서 article 부분을 news에 저장해서 뽑아 볼 수 있다.
-    console.log('rrr', data.articles);
+    console.log('rrr', news);
 };
 
 getNews();
