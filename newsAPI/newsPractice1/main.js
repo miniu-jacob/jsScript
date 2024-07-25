1057696d4c7b25aa4aa5c93d7538601264944891
miniu <miniu.media@gmail.com>
2024-07-25 17:47:24 +0700

    user별 settings.json file editing

--- newsAPI/newsPractice1/main.js
+++ newsAPI/newsPractice1/main.js
@@ -3,3 +3,23 @@
 const getLatestNews(){
 
 }
+
+
+document.getElementById('.abc')
+
+for (i = 0; i < 10; i++){
+  console.log('aaa');
+
+
+  document.getElementById()
+
+
+}
+
+
+console.log('aaa', param);
+
+function test() {
+  console.log('Hello')
+
+}
